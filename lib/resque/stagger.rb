df85676f598f97a38d9031df6787c159292edff7
Parikshit Singh <pk223933@gmail.com>
2023-01-31 00:05:09 +0530

    refactored

--- lib/resque/stagger.rb
+++ lib/resque/stagger.rb
@@ -8,19 +8,21 @@ module Resque
       end
 
       def enqueue(klass, *args)
-        ::Resque.enqueue_at(current_enqueue_at, klass, *args)
+        # ::Resque.enqueue_at(current_enqueue_at, klass, *args)
+        puts current_enqueue_at
       end
 
       private
 
       def current_enqueue_at
-        @__enqueued_last_second = @__enqueued_last_second.to_i + 1
         return starting_from if per_second.to_i.zero?
-        return @__last_enqueued_at = starting_from if @__last_enqueued_at.blank?
-        return @__last_enqueued_at if @__enqueued_last_second <= per_second
+        return @last_enqueued_at = starting_from if @last_enqueued_at.blank?
 
-        @__enqueued_last_second = 0
-        @__last_enqueued_at += 1.second
+        @enqueued_last_second = @enqueued_last_second.to_i + 1
+        return @last_enqueued_at if @enqueued_last_second < per_second
+
+        @enqueued_last_second = 0
+        @last_enqueued_at += 1.second
       end
 
       def starting_from
@@ -33,3 +35,5 @@ module Resque
     end
   end
 end
+
+Resque.prepend(Resque::Stagger)
